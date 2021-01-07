3a662b16f813362ff81a52249d64afdf2a25fa19
singhsaurabh <gsaurabh.singh@gmail.com>
2021-01-07 21:55:15 +0000

    Python log file

---
+++ log.py
@@ -0,0 +1,45 @@
+"""
+log.py : Create log file for current session.
+"""
+
+import logging
+
+
+filelogger = logging.getLogger()
+filelogger.setLevel(logging.DEBUG)
+
+switch_log_levels = {
+    'debug' : filelogger.debug,
+    'info' : filelogger.info,
+    'warning' : filelogger.warning,
+    'error' : filelogger.error,
+    'critical' : filelogger.critical 
+}
+
+def logsetup(logfile):
+    global filelogger
+
+    logFormatStr = "[%(asctime)s %(threadname)s, %(levelname)s] %(message)s"
+    consoleFormatStr = "[%(threadname)s, %(levelname)s] %(message)s"
+
+    # log file handler
+    logFormatter = logging.Formatter(logFormatStr)
+    fileHandler = logging.FileHandler(logfile)
+    fileHandler.setFormatter(logFormatter)
+    filelogger.addHandler(fileHandler)
+
+    # Stream handler for stdout, stderror
+    consoleFormatter = logging.Formatter(consoleFormatStr)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(consoleFormatter)
+    filelogger.addHandler(consoleHandler)
+
+def logevent(string, level='debug', print_screen=False, remove_newlines=False):
+
+    if remove_newlines:
+        string = string.replace('\r', '').replace('\n', ' ')
+
+    if print_screen:
+        switch_log_levels[level](string)
+
+    switch_log_levels[level](string)
